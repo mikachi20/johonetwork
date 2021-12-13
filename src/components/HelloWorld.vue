311ee1f75927f7e8f0b2e5702154ba7e2a7e8b96
mikachi20 <tomo.400m@icloud.com>
2021-12-13 10:43:32 +0900

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,59 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="app">
+    <h1>RakutenAPI練習</h1>
+    <div>
+      <button v-on:click="getLarge">料理別で調べる</button>|
+      <button v-on:click="getMedium"></button>|
+      <button v-on:click="getSmall">小さいレシピ</button>
+      <div>
+        {{ comment }}
+      </div>
+      <!-- <div v-for="info in infos" :key="info.id">
+        {{ info.snippet.description }}
+        <div>------</div>
+      </div> -->
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data() {
+    return {
+      infos: [],
+      comment: "",
+    }
+  },
+  methods: {
+    getLarge() {
+      this.getRakutenApi("large")
+    },
+    getMedium() {
+      this.getRakutenApi("medium")
+    },
+    getSmall() {
+      this.getRakutenApi("small")
+    },
+    async getRakutenApi(recipeType) {
+      const res = await fetch(
+        // "https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=1007014016464114143&categoryId=31"
+        "https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?applicationId=1007014016464114143&categoryType=" +
+          recipeType
+      )
+      try {
+        const value = await res.json()
+        console.log("value:" + JSON.stringify(value))
+        this.comment = value
+      } catch (e) {
+        console.log("error:" + e)
+      }
+    },
+  },
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+#app {
+  text-align: center;
 }
 </style>
